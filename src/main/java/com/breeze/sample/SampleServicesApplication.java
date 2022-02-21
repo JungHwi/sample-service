ad304db655a8cbf47ad027209c81df5a34bcaaa1
breeze <breeze@gripcorp.co>
2022-02-21 15:37:09 +0900

    first commit

---
+++ src/main/java/com/breeze/sample/SampleServicesApplication.java
@@ -0,0 +1,13 @@
+package com.breeze.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleServicesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/breeze/sample/SampleServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.breeze.sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleServicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
